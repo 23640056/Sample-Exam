958b419b34985d5c81b6f6728dca173774eedf66
Zarif <23640056@student.uwa.edu.au>
2023-05-27 18:51:36 +0800

    Notes area -> red (error)

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ function toggle(contentId) {
         content.style.display = 'none';
     }
 }
+
+function checkLength(textarea) {
+    var notes = textarea.value;
+    var maxLength = 1000;
+
+    if (notes.length > maxLength) {
+      textarea.parentNode.classList.add('error');
+    } 
+    else {
+      textarea.parentNode.classList.remove('error');
+    }
+}
